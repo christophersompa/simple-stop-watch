68347461e43cab6b716b19e8d03fccdc54111e49
Christopher <cgtsompa@gmail.com>
2024-10-11 18:10:29 +0100

    tested and working

--- simple-stop-watch/Program.cs
+++ simple-stop-watch/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,45 @@ namespace simple_stop_watch
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to my stopwatch application:");
+            
+            Stopwatch stopwatch = new Stopwatch();
+
+            bool isRunning = true;
+
+            while (isRunning == true) {
+
+
+                Console.WriteLine("Action: 1. Start | 2. Stop | 3. Reset | 4. Exit");
+
+                string action = Console.ReadLine();
+
+                if (action == "Start" || action == "1")
+                {
+                    stopwatch.Start();
+                    Console.WriteLine("Stopwatch has started!");
+                }
+                else if (action == "Stop" || action == "2")
+                {
+                    stopwatch.Stop();
+
+                    Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
+                }
+                else if (action == "Reset" || action == "3")
+                {
+                    stopwatch.Reset();
+                    Console.WriteLine("Stopwatch has been reset!");
+                }
+                else if (action == "Exit" || action == "4")
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input!");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
